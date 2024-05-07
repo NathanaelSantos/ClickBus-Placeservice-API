c98d463f53e5fd04590166ad8865cef79dd9ce72
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-07 04:44:42 -0300

    Add initial Spring Boot application setup for Place Service

---
+++ src/main/java/com/springboot/placeservice/PlaceserviceApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.placeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceserviceApplication.class, args);
+	}
+}
