8bb5791fffd5b288073867edf361729e7838e505
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-08 22:18:24 -0300

    Add interface PlaceRepositoy

--- src/main/java/com/springboot/placeservice/repositories/PlaceRepositoy.java
+++ src/main/java/com/springboot/placeservice/repositories/PlaceRepositoy.java
@@ -1,9 +1,9 @@
 package com.springboot.placeservice.repositories;
 
-import com.springboot.placeservice.models.PlaceModel;
+import com.springboot.placeservice.dtos.Place;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import java.util.UUID;
 
-public interface PlaceRepositoy extends ReactiveCrudRepository<PlaceModel, UUID> {
+public interface PlaceRepositoy extends ReactiveCrudRepository<Place, Long> {
 }
