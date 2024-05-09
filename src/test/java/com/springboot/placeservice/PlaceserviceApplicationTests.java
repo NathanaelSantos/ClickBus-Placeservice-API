4ec6b57a58ff6b6007567119088a013376381d15
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-09 00:18:33 -0300

    Added integration test for place creation in PlaceserviceApplicationTests

--- src/test/java/com/springboot/placeservice/PlaceserviceApplicationTests.java
+++ src/test/java/com/springboot/placeservice/PlaceserviceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.springboot.placeservice;
 
+import com.springboot.placeservice.apis.PlaceRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlaceserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	WebTestClient webTestClient;
 
+	@Test
+	public void testCreatePlaceSuccess(){
+		var name = "Valid Name";
+		var state = "Valid State";
+		var slug = "valid-name";
+
+		webTestClient
+				.post()
+				.uri("/places")
+				.bodyValue(new PlaceRequest(name, state))
+				.exchange()
+				.expectBody()
+				.jsonPath("name").isEqualTo(name)
+				.jsonPath("state").isEqualTo(state)
+				.jsonPath("slug").isEqualTo(slug)
+				.jsonPath("createdAt").isNotEmpty()
+				.jsonPath("updatedAt").isNotEmpty();
+
+	}
 }
