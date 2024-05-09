a6068f45a840ee4d3bf902b6dc9a4846ae30ef8b
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-08 22:23:18 -0300

    Add schema sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS place (
+    id bigint AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    slug VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
