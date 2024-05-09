aa377cabc7d7f0f6fc208606dc608cf5e05a2058
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-08 22:22:39 -0300

    Add class PlaceMapper

---
+++ src/main/java/com/springboot/placeservice/mappers/PlaceMapper.java
@@ -0,0 +1,10 @@
+package com.springboot.placeservice.mappers;
+
+import com.springboot.placeservice.apis.PlaceResponse;
+import com.springboot.placeservice.dtos.Place;
+
+public class PlaceMapper {
+    public static PlaceResponse fromPlaceToResponse(Place place){
+        return new PlaceResponse(place.name(), place.slug(), place.state(), place.createdAt(), place.updatedAt());
+    }
+}
