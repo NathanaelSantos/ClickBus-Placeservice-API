1545a7170b1567dd243bbd0a87e4d742a4c90e7f
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-08 22:18:46 -0300

    Add classe PlaceService

--- src/main/java/com/springboot/placeservice/services/PlaceService.java
+++ src/main/java/com/springboot/placeservice/services/PlaceService.java
@@ -1,11 +1,19 @@
 package com.springboot.placeservice.services;
 
+import com.springboot.placeservice.apis.PlaceRequest;
+import com.springboot.placeservice.dtos.Place;
 import com.springboot.placeservice.repositories.PlaceRepositoy;
+import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepositoy repositoy;
 
     public PlaceService(PlaceRepositoy repositoy) {
         this.repositoy = repositoy;
+    }
+
+    public Mono<Place> create(PlaceRequest placeRequest) {
+        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
+        return repositoy.save(place);
     }
 }
