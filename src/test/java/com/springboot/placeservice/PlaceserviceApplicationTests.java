45f91e2caae3da625e92a930acbac2762d12c7df
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-08 22:23:30 -0300

    Add main class

---
+++ src/test/java/com/springboot/placeservice/PlaceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.placeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
