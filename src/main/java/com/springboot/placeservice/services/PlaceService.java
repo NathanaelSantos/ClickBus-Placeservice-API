67b5a4ca75f949fa4999511987fda257a675b175
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-08 23:37:24 -0300

    Integrated Slugify into PlaceService for automatic slug generation from place name

--- src/main/java/com/springboot/placeservice/services/PlaceService.java
+++ src/main/java/com/springboot/placeservice/services/PlaceService.java
@@ -1,5 +1,6 @@
 package com.springboot.placeservice.services;
 
+import com.github.slugify.Slugify;
 import com.springboot.placeservice.apis.PlaceRequest;
 import com.springboot.placeservice.dtos.Place;
 import com.springboot.placeservice.repositories.PlaceRepositoy;
@@ -7,13 +8,15 @@ import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepositoy repositoy;
+    private Slugify slfy;
 
     public PlaceService(PlaceRepositoy repositoy) {
         this.repositoy = repositoy;
+        this.slfy = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest) {
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
+        var place = new Place(null, placeRequest.name(), slfy.slugify(placeRequest.name()), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
         return repositoy.save(place);
     }
 }
