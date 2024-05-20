a81c354e9e1a84bdf1f89f3dc8c1b90cb74097bb
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-20 12:15:04 -0300

    Added Edit a place, Get a specific place, List places

--- src/main/java/com/springboot/placeservice/controllers/PlaceController.java
+++ src/main/java/com/springboot/placeservice/controllers/PlaceController.java
@@ -2,16 +2,13 @@ package com.springboot.placeservice.controllers;
 
 import com.springboot.placeservice.apis.PlaceRequest;
 import com.springboot.placeservice.apis.PlaceResponse;
-import com.springboot.placeservice.dtos.Place;
 import com.springboot.placeservice.mappers.PlaceMapper;
 import com.springboot.placeservice.services.PlaceService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -24,8 +21,37 @@ public class PlaceController {
     }
 
     @PostMapping
-    public ResponseEntity<Mono<PlaceResponse>> create(@Valid @RequestBody PlaceRequest request){
-        var placeResponseMono = placeService.create(request).map(PlaceMapper::fromPlaceToResponse);
-        return ResponseEntity.status(HttpStatus.CREATED).body(placeResponseMono);
+    public ResponseEntity<Mono<PlaceResponse>> createPlace(@Valid @RequestBody PlaceRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                placeService.create(request).map(PlaceMapper::fromPlaceToResponse)
+        );
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<Flux<PlaceResponse>> getAll() {
+        return ResponseEntity.ok().body(
+                placeService.getAll().map(PlaceMapper::fromPlaceToResponse)
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Mono<PlaceResponse>> getPlaceId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(
+                placeService.findById(id).map(PlaceMapper::fromPlaceToResponse)
+        );
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Mono<PlaceResponse>> getPlace(@PathVariable String name) {
+        return ResponseEntity.ok().body(
+                placeService.findByName(name).map(PlaceMapper::fromPlaceToResponse)
+        );
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> updatePlace(@PathVariable Long id, @RequestBody PlaceRequest newPlace) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                placeService.updatePlace(id, newPlace).map(PlaceMapper::fromPlaceToResponse)
+        );
     }
 }
