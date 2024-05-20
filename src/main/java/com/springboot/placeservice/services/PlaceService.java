dfe5faca6e4446c7db48e2bbbd179d6ba5a21011
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-20 12:16:50 -0300

    Added services for Edit a place ,Get a specific, place, List places

--- src/main/java/com/springboot/placeservice/services/PlaceService.java
+++ src/main/java/com/springboot/placeservice/services/PlaceService.java
@@ -4,24 +4,58 @@ import com.github.slugify.Slugify;
 import com.springboot.placeservice.apis.PlaceRequest;
 import com.springboot.placeservice.dtos.Place;
 import com.springboot.placeservice.repositories.PlaceRepositoy;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
-    private PlaceRepositoy repositoy;
+    private PlaceRepositoy repository;
     private Slugify slfy;
 
     public PlaceService(PlaceRepositoy repositoy) {
-        this.repositoy = repositoy;
+        this.repository = repositoy;
         this.slfy = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest) {
-        var place = new Place(null,
-                placeRequest.name(),
-                slfy.slugify(placeRequest.name()),
-                placeRequest.state(),
-                null,
-                null);
-        return repositoy.save(place);
+        return repository.save(
+                new Place(null,
+                        placeRequest.name(),
+                        slfy.slugify(placeRequest.name()),
+                        placeRequest.state(),
+                        null,
+                        null)
+        );
+    }
+
+    public Flux<Place> getAll() {
+        return repository.findAll();
+    }
+
+    public Mono<Place> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Mono<Place> findByName(String name) {
+        return repository.findByName(name);
+    }
+
+    public Mono<Place> updatePlace(Long id, PlaceRequest newPlace) {
+        return repository.findById(id)
+                .flatMap(existingPlace -> updatePlace(existingPlace, newPlace))
+                .switchIfEmpty(Mono.error(new ChangeSetPersister.NotFoundException()));
+    }
+
+    private Mono<Place> updatePlace(Place existingPlace, PlaceRequest newPlace) {
+        return repository.save(
+                new Place(
+                        existingPlace.id(),
+                        newPlace.name() != null ? newPlace.name() : existingPlace.name(),
+                        slfy.slugify(newPlace.name()),
+                        newPlace.state() != null ? newPlace.state() : existingPlace.state(),
+                        existingPlace.createdAt(),
+                        existingPlace.updatedAt()
+                )
+        );
     }
 }
