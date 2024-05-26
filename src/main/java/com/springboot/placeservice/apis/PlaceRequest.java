000b361a9a80fb4bcbdb0da9773cbbba5cf58aa8
Nathan Santos <nathanaelsantos15@gmail.com>
2024-05-26 06:16:47 -0300

    Validation for inputs requests

--- src/main/java/com/springboot/placeservice/apis/PlaceRequest.java
+++ src/main/java/com/springboot/placeservice/apis/PlaceRequest.java
@@ -3,4 +3,10 @@ package com.springboot.placeservice.apis;
 import jakarta.validation.constraints.NotBlank;
 
 public record PlaceRequest(@NotBlank String name, @NotBlank String state) {
+    public PlaceRequest {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        if (state == null || state.isBlank())
+            throw new IllegalArgumentException("State cannot be null or empty");
+    }
 }
